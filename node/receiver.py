cfd476fd116031e637693b515b098a4d5b541ae4
jleeong <jared.leeong13@gmail.com>
2018-02-24 12:50:35 -0800

    partial class definitions w/ comments

--- node/receiver.py
+++ node/receiver.py
@@ -2,3 +2,17 @@ class Receiver:
 	"""Class to encompass the TCP listening functions of the P2P node."""
 	def __init__(self):
 		print("Receiver created")
+	def listen(portnum):
+		"""listen places the Receiver object in an execution loop to listen
+		for incoming TCP requests to the designated port. After receiving a
+		request, it will determine the current node has the desired resource
+		and respond accordingly"""
+
+	def parseRequest(tcp_request):
+		"""parseRequest will parse the received TCP request and return an
+		array containing relevant details for further processing"""
+
+	def respond(request_details):
+		"""respond will examine the supplied request_details (supplied by
+		 parseRequest) and determine the proper action to take. (Send requested
+		 file or ignore the request)
--- node/sender.py
+++ node/sender.py
@@ -1,5 +1,9 @@
 class Sender:
 	"""Class to encompass the sending functions of a P2P node"""
-
 	def __init__(self):
 		print("Sender created")
+
+	def sendFile(destination_info, file_details):
+		"""sendFile will construct a TCP connection to the designated
+		destination, retrieve the resource defined by file details,
+		and perform the data transfer."""
