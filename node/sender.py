04de769b76314af0bbba2886146a76e2b81c8c2e
jleeong <jared.leeong13@gmail.com>
2018-02-25 12:40:28 -0800

    sender constructor

--- node/sender.py
+++ node/sender.py
@@ -1,11 +1,19 @@
 from .actor import Actor
+from .exceptions import no_file_dir
+import os
 class Sender(Actor):
 	"""Class to encompass the sending functions of a P2P node"""
 	def __init__(self):
 		"""Constructor for the Sender class. Needs to scan local filesystem
 		to determine what data exists on current node. Uses this information
 		to resopnd to P2P requests"""
-		print("	Sender created")
+		if os.path.isdir("files"):
+			self.available_files = os.listdir("files")
+			print("	Sender created")
+		else:
+			exception = no_file_dir.NoFileDir()
+			print(exception.__str___())
+			raise exception
 
 	def act(self,actionString,args):
 		"""act implementation of Actor superclass"""
