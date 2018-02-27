4e30355d7fddbdd3d6ac79b5880c3a9c44a153b3
jleeong <jared.leeong13@gmail.com>
2018-02-26 21:58:59 -0800

    fix image build

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ WORKDIR /var/cs176/p2p
 RUN mkdir -p files && touch hosts
 COPY . /var/cs176/p2p
 
-CMD ["python", "/var/cs176/runnode.py", "g", "daemon"]
+CMD ["python", "/var/cs176/p2p/runnode.py", "g", "daemon"]
--- docker.sh
+++ docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 docker_image=cs176b-p2p
 docker_network=p2p_nw
+count=2
+
 if [ "$(docker images -q $docker_image)" ==  "" ]
 then
     docker build -t $docker_image .
@@ -11,9 +13,8 @@ then
     docker network create p2p_nw
 fi
 
-count=10
 for i in `seq 1 $count`;
 do
     echo "docker run --name node-$i -d --network $docker_network $docker_image"
-    docker run --name node-$i -d --network $docker_network $docker_image
+    docker run --rm --name node-$i -d --network $docker_network $docker_image
 done
