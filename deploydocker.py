94679419623e68c373bbc8ea85f7b4fb95d95d30
jleeong <jared.leeong13@gmail.com>
2018-02-26 22:58:34 -0800

    pretty prompts and subprocess run fix

--- deploydocker.py
+++ deploydocker.py
@@ -16,6 +16,7 @@ if  docker_nw not in existing_nw:
     subprocess.run(['docker','network','create',docker_nw])
 
 for c in containers:
+    print(c)
     subprocess.run(['docker','run','--name',c,'-d','--network',docker_nw,\
-        '-v'+os.getcwd()+'/test_data/networking'+c+'hosts:/var/cs176/p2p/hosts',\
+        '-v'+os.getcwd()+'/test_data/networking/'+c+'.hosts:/var/cs176/p2p/hosts',\
         docker_image])
