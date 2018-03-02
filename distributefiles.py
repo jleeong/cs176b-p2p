9c11fd9eb6fb4d8a0c2e0fc3ddfc1ebe9df3a528
jleeong <jared.leeong13@gmail.com>
2018-03-02 10:41:31 -0800

    verbose option

--- distributefiles.py
+++ distributefiles.py
@@ -10,20 +10,25 @@ with open('test_data/nodes.json','r') as nodes:
     raw = ''.join(nodes.readlines())
     containers = json.loads(raw)
 
-if len(sys.argv) != 2:
-    print("Usage: python3 distributefiles.py <file-percentage>")
+if not len(sys.argv) >= 2:
+    print("Usage: python3 distributefiles.py <file-percentage> [-v]")
     sys.exit("ERROR: Unrecognized parameters")
 
 file_percent = sys.argv[1]
+verbose = len(sys.argv) == 3 and sys.argv[2]=='-v'
+active_files = []
 
 files = os.listdir('test_data/samples')
 for c in containers:
-    print(c+":")
+    if verbose: print(c+":")
     # copy over sample files based on file percentage
     for targetfile in files:
         if random.randint(1,100) <= int(file_percent):
-            print(" Copying sample file: "+targetfile)
+            if verbose: print(" Copying sample file: "+targetfile)
+            active_files.append(targetfile)
             subprocess.run(['docker','cp','test_data/samples/'+targetfile ,\
                 c+':/var/cs176/p2p/files/'+targetfile])
 
-print("Done.")
+for f in set(active_files):
+    print(f)
+if verbose: print("Done.")
