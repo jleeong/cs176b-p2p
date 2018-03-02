e37b7d1cce215282eed4c2c78a74798e3cf92479
jleeong <jared.leeong13@gmail.com>
2018-03-02 11:21:32 -0800

    fixed network generation

--- genhostfiles.py
+++ genhostfiles.py
@@ -1,41 +1,55 @@
 """This file will be used to generate the `hosts` files to place into the virtual nodes.
-It attempts to create an acyclic non-directional graph.
+It attempts to create a fully-connected non-directional graph with x amount of connections.
+per network node.
     for every node-x in `nodes.yml`:
         create `networking/node-x.hosts`"""
 import json
 import random
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 genhostfiles.py <node_connections>")
 
 with open("test_data/nodes.json",'r') as nodes:
     raw = ''.join(nodes.readlines())
-    workingset = json.loads(raw)
-print(workingset)
+    initialset = json.loads(raw)
+print(initialset)
 hostfiles = {}
 ir = {}
-total = len(workingset)
+total = len(initialset)
+num_connections = int(sys.argv[1])
+if(num_connections > total):
+    sys.exit("Too many per node connections.")
 print(str(total)+" nodes found.")
 try:
     # make host files
-    for n in workingset:
+    for n in initialset:
         hostfiles[n] = open('test_data/networking/'+n+'.hosts','w')
         ir[n] = []
 
     # populate intermediate representation
-    for i,node in enumerate(ir):
-        if(total==1):
-            numhosts = 1
-        else:
-            numhosts = random.randint(0,total-1)
-        print("Networking "+node+". Adding "+str(numhosts)+" connections...")
-        while len(workingset)>0:
-            if len(ir[node]) >= numhosts: break
-            index = random.randint(0,total-1)
-            if not workingset[index] == node:
-                print(" Adding "+workingset[index])
-                ir[node].append(workingset[index])
-                ir[workingset[index]].append(node)
-                del(workingset[index])
+    for i in range(num_connections):
+        print("Connection: "+str(i))
+        workingset = initialset[:]
+        networkset = []
+        currnode = workingset[0]
+        networkset.append(currnode)
+        while len(workingset)>1:
+            avail = set(workingset) - set(networkset)
+            #print(currnode, ir[currnode], avail)
+            target = list(avail)[random.randint(1,len(avail))-1]
+            print(" Adding "+currnode+"->"+target+" to network.")
+            ir[currnode].append(target)
+            ir[target].append(currnode)
+            workingset.remove(target)
+            networkset.append(target)
+            currnode=target
+        # last non-connected node
+        target = workingset[0]
+        print("Adding "+currnode+"->"+target+" to network.")
+        ir[currnode].append(target)
+        ir[target].append(currnode)
 
-            total = len(workingset)
     print("Final network:")
 finally:
     # write and close host files
