bd2ee222f7655ef131b25641942a34bbdd1b3bf1
millan batra <millanbatra@umail.ucsb.edu>
2018-03-08 22:47:44 -0800

    added flag to distribute files according to dht method

--- distributefiles.py
+++ distributefiles.py
@@ -6,6 +6,8 @@ import random
 import subprocess
 import os
 import sys
+import hashlib
+
 with open('test_data/nodes.json','r') as nodes:
     raw = ''.join(nodes.readlines())
     containers = json.loads(raw)
@@ -13,21 +15,53 @@ with open('test_data/nodes.json','r') as nodes:
 if not len(sys.argv) >= 2:
     print("Usage: python3 distributefiles.py <file-percentage> [-v]")
     sys.exit("ERROR: Unrecognized parameters")
+verbose = ''
+verbose = len(sys.argv) == 3 and sys.argv[2]=='-v'
+
+mode = '-g'
+if(len(sys.argv) == 4):
+    if(sys.argv[2]=='-v'):
+        verbose = True
+    mode = sys.argv[3]
+
+    if(mode !='-d' and mode!='-g'):
+        print("Usage: python3 distributefiles.py <file-percentage> [-v] [-d or -g]")
+        sys.exit("ERROR: Unrecognized parameters")
 
 file_percent = sys.argv[1]
-verbose = len(sys.argv) == 3 and sys.argv[2]=='-v'
+
+
 active_files = []
 
-files = os.listdir('test_data/samples')
-for c in containers:
-    if verbose: print(c+":")
-    # copy over sample files based on file percentage
+if(mode == '-g'):
+    print("distributing randomly as per gnutella routing")
+    files = os.listdir('test_data/samples')
+    for c in containers:
+        if verbose: print(c+":")
+        # copy over sample files based on file percentage
+        for targetfile in files:
+            if random.randint(1,100) <= int(file_percent):
+                if verbose: print(" Copying sample file: "+targetfile)
+
+                active_files.append(targetfile)
+                subprocess.run(['docker','cp','test_data/samples/'+targetfile ,\
+                    c+':/var/cs176/p2p/files/'+targetfile])
+else:#mode = '-d'
+    print("distributing hash(files)modulo #numnodes according to distributed hash tables initialization")
+    files = os.listdir('test_data/samples')
+    num_nodes = len(containers) #used for modulo in hash_function
+    #hash the
     for targetfile in files:
-        if random.randint(1,100) <= int(file_percent):
-            if verbose: print(" Copying sample file: "+targetfile)
-            active_files.append(targetfile)
-            subprocess.run(['docker','cp','test_data/samples/'+targetfile ,\
-                c+':/var/cs176/p2p/files/'+targetfile])
+        #compute docker container in which we will copy the file
+        m = hashlib.md5(targetfile.encode('utf-8'))
+        z = int(m.hexdigest(), 16)
+        container_number = z%num_nodes
+        if verbose: print(containers[container_number]+":")
+        # copy over file to container based on hash result
+        if verbose: print(" Copying sample file: "+targetfile)
+        active_files.append(targetfile)
+        subprocess.run(['docker','cp','test_data/samples/'+targetfile ,\
+            containers[container_number]+':/var/cs176/p2p/files/'+targetfile])
 
 for f in set(active_files):
     print(f)
