3b05bc43aa0dceea074f04cd6ef35e365a4bd09d
jleeong <jared.leeong13@gmail.com>
2018-03-10 12:30:44 -0800

    change from append functionality to set count functionality

--- append_nodes.py
+++ append_nodes.py
@@ -7,12 +7,9 @@ if not len(sys.argv) >= 2:
 
 num_nodes = int(sys.argv[1])
 
-with open("test_data/nodes.json",'r') as nodes:
-    raw = ''.join(nodes.readlines())
-    initialset = json.loads(raw)
-initial_length = len(initialset)
-for i in range(0, num_nodes): 
-    initialset.append("node-%s"% str(i+initial_length))
+initialset = []
+for i in range(0, num_nodes):
+    initialset.append("node-%s"% str(i))
 
-with open("test_data/nodes.json",'w') as nodes: 
+with open("test_data/nodes.json",'w') as nodes:
   json.dump(initialset, nodes)
